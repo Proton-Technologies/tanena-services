721b10a48d045dbfa09dd07d9919c35893cb2248
fortune710 <fortune0208@yahoo.com>
2023-01-02 18:38:34 +0100

    added tailwind css config and deps

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': "#48BF91",
+        'background': "#002D90"
+      }
+    },
+  },
+  plugins: [],
+}
