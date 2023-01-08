b3709d489f0910a2761fe1c3effb34b0e3a58941
fortune710 <fortune0208@yahoo.com>
2023-01-08 16:48:20 +0100

    changes

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -3,13 +3,20 @@ interface ButtonProps {
     endIcon?: any,
     children: React.ReactNode,
     variant?: 'contained'|'outlined',
-    className?: string
+    className?: string,
+    onClick?: React.MouseEventHandler,
+    type?: 'button'|'submit'|'reset'
 }
 
-export const Button: React.FC<ButtonProps> = ({ startIcon, endIcon, children, variant, className }) => {
+export const Button: React.FC<ButtonProps> = ({ type, onClick, startIcon, endIcon, children, variant, className }) => {
     const variantStyles = variant === 'contained' ? 'bg-primary text-white' : 'border-2 border-primary text-primary'
     return(
-        <button className={`flex px-4 py-2 gap-2 items-center rounded-md font-normal ${className} ${variantStyles}`}>
+        <button 
+            onClick={onClick} 
+            type={type} 
+            className={`flex px-4 py-2 gap-2 items-center rounded-md 
+            font-normal ${className} ${variantStyles}`}
+        >
             {startIcon}
             {children}
             {endIcon}
